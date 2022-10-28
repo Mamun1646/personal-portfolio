8d23e1938a8879c33be6042d73a2469cb45bf7e5
Mamun1646 <mimbd0113000gmail.com>
2022-10-28 12:57:58 +0600

    Personal Portfolio

---
+++ src/data.js
@@ -0,0 +1,50 @@
+
+export const projects = [
+  {
+    title: "The Quran Analyzer",
+    subtitle: "Next.js Nestjs Projects",
+    description:
+      "This website builds using Tailwind CSS, Next.js, Nest.js, Graphql, and MongoDB. There are many features available on this website. People can learn, read, and listen with translation and transliteration. I mention that there are more than five languages available in this section.",
+    image: "./the-quran-analyzer.gif",
+    link: "https://the-quran-analyzer.netlify.app/",
+  },
+  {
+    title: "Personal Porfolio",
+    subtitle: "React Taliwind CSS",
+    description:
+      " This website builds using Tailwind CSS, Next.js, Nest.js, Graphql, and MongoDB. There are many features available on this website Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://www.linkedin.com/in/almamunmim1611177146/",
+  },
+  {
+    title: "Personal Porfolio",
+    subtitle: "React Taliwind CSS",
+    description:
+      " This website builds using Tailwind CSS, Next.js, Nest.js, Graphql, and MongoDB. There are many features available on this website Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://www.linkedin.com/in/almamunmim1611177146/",
+  },
+  {
+    title: "Personal Porfolio",
+    subtitle: "React Taliwind CSS",
+    description:
+      " This website builds using Tailwind CSS, Next.js, Nest.js, Graphql, and MongoDB. There are many features available on this website Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://www.linkedin.com/in/almamunmim1611177146/",
+  },
+];
+
+
+
+export const skills = [
+  "HTML",
+  "Tailwind CSS",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Nest.js",
+  "Mongodb",
+  "MySQL"
+];
